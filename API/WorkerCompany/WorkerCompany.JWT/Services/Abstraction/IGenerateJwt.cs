7757636654fdabe05a8891559e288158d85f20be
Ethriel <bartolomio177@gmail.com>
2020-12-28 15:29:45 +0200

    added: JWT generator

---
+++ API/WorkerCompany/WorkerCompany.JWT/Services/Abstraction/IGenerateJwt.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using WorkerCompany.Authentication.Models;
+
+namespace WorkerCompany.JWT.Services.Abstraction
+{
+    public interface IGenerateJwt
+    {
+        Task<string> CreateToken(AppUser user);
+    }
+}
