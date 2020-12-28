e1526f9a84a05c4894601ebfbc851c81c590422c
Ethriel <bartolomio177@gmail.com>
2020-12-28 16:54:56 +0200

    addedL auth seeder

---
+++ API/WorkerCompany/WorkerCompany.Authentication/AuthItems/AuthSeeder.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkerCompany.Authentication.Models;
+
+namespace WorkerCompany.Authentication.AuthItems
+{
+    public class AuthSeeder
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<AppUser> userManager;
+
+        public AuthSeeder(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+        }
+
+        public async Task CreateRoles()
+        {
+            await CreateRole(Roles.Admin);
+            await CreateRole(Roles.User);
+        }
+
+        public async Task CreateUsers()
+        {
+            await CreateUser("App admin", "admin@gmail.com", "Qwerty-1", Roles.Admin);
+            await CreateUser("App test user", "testuser@gmail.com", "Qwerty-1", Roles.User);
+        }
+
+        private async Task CreateRole(string name)
+        {
+            var existingRole = roleManager.Roles.Any(r => r.Name.Equals(name));
+
+            if (!existingRole)
+            {
+                var role = new IdentityRole
+                {
+                    Name = name,
+                    NormalizedName = name.ToUpper()
+                };
+                var result = await roleManager.CreateAsync(role);
+            }
+        }
+
+        private async Task CreateUser(string displayName, string email, string password, string role)
+        {
+            var existingUser = userManager.Users.Any(u => u.Email.Equals(email));
+
+            if (!existingUser)
+            {
+                var user = new AppUser
+                {
+                    DisplayName = displayName,
+                    Email = email,
+                    EmailConfirmed = true
+                };
+                var result = await userManager.CreateAsync(user, password);
+                await AttachUserToRole(user, role);
+            }
+        }
+
+        private async Task AttachUserToRole(AppUser user, string role)
+        {
+            var userRoles = await userManager.GetRolesAsync(user);
+
+            var existingRole = userRoles.Any(r => r.Equals(role));
+
+            if (!existingRole)
+            {
+                var result = await userManager.AddToRoleAsync(user, role);
+            }
+        }
+    }
+}
